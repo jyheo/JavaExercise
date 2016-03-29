7cba57a99d1e8f32f81f726565245f577e17e9de
chaminhye <minhye_love@naver.com>
2016-03-29 14:32:27 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,44 +1,76 @@
-package classes;
+
 
 public class Rectangle {
     private int x1, y1, x2, y2;
 
     public void setPoints(int _x1, int _y1, int _x2, int _y2) {
         // TODO, check if x, y > 0
-        x1 = _x1;
-        y1 = _y1;
-        x2 = _x2;
-        y2 = _y2;
+
+        if(_x1>0 && _x2>0 && _y1 >0 && _y2>0) {
+            if(_x1<_x2 && _y1<_y2){
+                x1 = _x1;
+                y1 = _y1;
+                x2 = _x2;
+                y2 = _y2;
+            }
+            else if(_x1>_x2 && _y1<_y2){
+                x1 = _x2;
+                y1 = _y1;
+                x2 = _x1;
+                y2 = _y2;
+            }
+            else if(_x1>_x2 && _y1>_y2){
+                x1 = _x2;
+                y1 = _y2;
+                x2 = _x1;
+                y2 = _y1;
+            }
+            else{
+                x1 = _x1;
+                y1 = _y2;
+                x2 = _x2;
+                y2 = _y1;
+            }
+        }
     }
 
     public int getWidth() {
-        // TODO
-        return 0;
+        // 가로길이 구하기
+        return Math.abs(x2-x1);
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+        // 세로길이 구하기
+        return Math.abs(y2-y1);
     }
 
     public int getArea() {
-        // TODO
-        return 0;
+        // 넓이면적 구하기
+        return this.getHeight() * this.getWidth();
     }
 
     public boolean equals(Rectangle r) {
-        // TODO
-        return false;
+        // 두 개의 사각형이 일치하는 경우
+        if(x1 == r.x1 && x2 == r.x2 && y1 == r.y1 && y2 == r.y2 )
+            return true;
+        else
+            return false;
     }
 
     public boolean in(Rectangle r) {
-        // TODO
-        return false;
+        // 한 개의 사각형안에 다른 사각형이 포함되는 경우
+        if(x1 <= r.x1 && x2 <= r.x2 && y1 <= r.y1 && y2 <= r.y2 )
+            return true;
+        else
+            return false;
     }
 
     public boolean overlap(Rectangle r) {
-        // TODO
-        return false;
+        //두 개의 사각형이 겹치는 경우
+        if(x1 != r.x1 && x2 != r.x2 && y1 != r.y1 && y2 != r.y2 )
+            return true;
+        else
+            return false;
     }
 
     public static void main(String[] args) {
@@ -66,5 +98,4 @@ public class Rectangle {
             System.out.println("r1 and r4 overlap.");
         }
     }
-
 }
