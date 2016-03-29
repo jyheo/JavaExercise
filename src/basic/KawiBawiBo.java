d77c1834657e8ee07b4dfb6fa1c4d0e917bf63ef
chaminhye <minhye_love@naver.com>
2016-03-29 14:05:52 +0900

    Update KawiBawiBo.java

--- src/basic/KawiBawiBo.java
+++ src/basic/KawiBawiBo.java
@@ -1,13 +1,8 @@
-package basic;
-
 import java.util.Scanner;
 
-/**
- * Created by jyheo on 2016-03-10.
- */
-public class KawiBawiBo {
-    public static boolean isKawiBawiBoSwitch(String str) {
-        switch (str) {
+public class hello{
+    public static boolean iskawibawiboSwitch(String str){
+        switch(str){
             case "가위":
             case "바위":
             case "보":
@@ -17,36 +12,54 @@ public class KawiBawiBo {
         }
     }
 
-    public static boolean isKawiBawiBoIf(String str) {
-        if (str.equals("가위") || str.equals("바위") || str.equals("보"))
+    /*public static boolean iskawibawiboIf(String str){
+        if(str.equals("가위")||str.equals("바위")||str.equals("보"))
             return true;
         else
             return false;
-        // this if statement can be simplified such as
-        // return (str.equals("가위") || str.equals("바위") || str.equals("보"));
-    }
-
+    }*/
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Bob >> ");
+        System.out.print("Bob>> ");
         String bob = scanner.next();
-        if (!isKawiBawiBoSwitch(bob)) {
-            System.out.println("Input allowed only : 가위, 바위, 보");
+        if (!iskawibawiboSwitch(bob)) {
+            System.out.println("input allowed only : 가위, 바위, 보");
             return;
         }
-        System.out.print("Alice >> ");
-        String alice = scanner.next();
-        if (!isKawiBawiBoIf(alice)) {
-            System.out.println("Input allowed only : 가위, 바위, 보");
-            return;
+            System.out.print("Alice>> ");
+            String alice = scanner.next();
+            if (!iskawibawiboSwitch(alice)) {
+                System.out.println("input allowed only : 가위, 바위, 보");
+                return;
+
+            }
+
+            if(bob.equals("가위")){
+                if(alice.equals("가위"))
+                    System.out.println("Tie");
+                else if(alice.equals("바위"))
+                    System.out.println("alice win");
+                else
+                    System.out.println("bob win");
+            }
+            else if(bob.equals("바위")){
+                if(alice.equals("가위"))
+                    System.out.println("bob win");
+                else if(alice.equals("보"))
+                    System.out.println("alice win");
+                else
+                    System.out.println("bob win");
+            }
+            else{
+                if(alice.equals("가위"))
+                    System.out.println("alice win");
+                else if(alice.equals("바위"))
+                    System.out.println("bob win");
+                else
+                    System.out.println("bob win");
+            }
+
+
         }
-
-        // TODO: determine who win using if/else
-
-        // output will be one of :
-        System.out.println("Alice win!");
-        System.out.println("Bob win!");
-        System.out.println("Tie!");
     }
-}
