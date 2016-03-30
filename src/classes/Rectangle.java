fdb2ee10f90661e9770adc0691ca7caec8634fdf
black5098 <jongho6345@naver.com>
2016-03-31 00:15:07 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,55 +1,79 @@
 package classes;
 
-public class Rectangle {
-    private int x1, y1, x2, y2;
+public class Hello {
+    private int x1,x2,y1,y2;
 
-    public void setPoints(int _x1, int _y1, int _x2, int _y2) {
-        // TODO, check if x, y > 0
-        x1 = _x1;
-        y1 = _y1;
-        x2 = _x2;
-        y2 = _y2;
-    }
+    public void setPoints(int _x1, int _y1, int _x2, int _y2) {//r1  값을 받음
+      if(_x1>_x2 && _x1>0 &&_x2>0)
+      {
+          x2 =_x1;
+          x1 =_x2;
+      }
+        else if (_x1<_x2&& _x1>0 &&_x2>0)
+      {
+          x1=_x1 ;
+          x2=_x2 ;
+
+      }
+        if(_y1>_y2 && _y1>0 &&_y2>0)
+        {
+            y2=_y1;
+            y1=_y2;
+        }
+        else if(_x1<_x2 && _x1>0 &&_x2>0)
+        {
+            y1=_y1  ;
+            y2=_y2;
+        }
+}
 
     public int getWidth() {
-        // TODO
-        return 0;
+        return (x2 - x1);
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+
+        return (y2 - y1);
     }
 
     public int getArea() {
-        // TODO
-        return 0;
+
+        return (x2 - x1) * (y2 - y1);
     }
 
-    public boolean equals(Rectangle r) {
-        // TODO
-        return false;
+    public boolean equals(Hello r) {
+        if ((x1 == r.x1) && (x2 == r.x2) && (y1 == r.y1) && (y2 == r.y2))
+            //인자로 받은 좌표값 r1과 r2 값이 동일한지 비교.
+            return true;
+        else
+            return false;
     }
 
-    public boolean in(Rectangle r) {
-        // TODO
-        return false;
+    public boolean in(Hello r) {
+        if ((r.x1 <= x1 && x1 <= r.x2) && (r.x1 <= x2 && x2 <= r.x2) && (r.y1 <= y1 && y1 <= r.y2) && (r.y1 <= y2 && y2 <= r.y2))
+            // 인자로 받은 r1 좌표값이 해당 r3 값보다 크거다 같은지 비교
+            return true;
+        else
+            return false;
     }
 
-    public boolean overlap(Rectangle r) {
-        // TODO
+    public boolean overlap(Hello r) {
+        if (((x1 <= r.x1 && r.x1 <= x2) || (x1 <= r.x2 && r.x2 <= x2)) && ((y1 <= r.y1 && r.y1 <= y2) || (y1 <= r.y2 && r.y2 <= y2)))
+            // 인자로 받은 좌표값 r1과 r4가 겹쳐있는지 비교
+            return true;
+        else
         return false;
     }
 
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle();
-        Rectangle r3 = new Rectangle();
-        Rectangle r4 = new Rectangle();
+        Hello r1 = new Hello();
+        Hello r2 = new Hello();
+        Hello r3 = new Hello();
+        Hello r4 = new Hello();
 
-        r1.setPoints(1, 1, 5, 5);
+        r1.setPoints(3,6,5,10);
         r2.setPoints(5, 5, 1, 1);
-        r3.setPoints(1, 1, 6, 6);
+        r3.setPoints(1,1 ,5 ,5 );
         r4.setPoints(3, 3, 7, 8);
 
         System.out.println(r1.getWidth());
@@ -68,3 +92,4 @@ public class Rectangle {
     }
 
 }
+
