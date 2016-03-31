bbb1cf8d6b4b9714b85e42d668e6dbf84a3cf2ab
yoobolam <ybr0213@naver.com>
2016-03-31 20:44:03 +0900

    Update Sum2.java

--- src/basic/Sum2.java
+++ src/basic/Sum2.java
@@ -1,19 +1,16 @@
-package basic;
-
 import java.util.Scanner;
-
-public class Sum2 {
-    public static void main(String[] args) {
-        float fSum = 0;
-        int iNum = 0;
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (scanner.hasNextInt()) {
-            fSum += scanner.nextFloat();
-            iNum++;
+/**
+ * Created by 유오재 on 2016-03-31.
+ */
+public class A2 {
+    public static void main(String[] args){
+        float a=0;
+        int b=0;
+        Scanner aaa = new Scanner(System.in);
+        while(aaa.hasNextInt()){
+            a += aaa.nextFloat();
+            b++;
         }
-
-        System.out.println(fSum / iNum);
+        System.out.println("입력된값의 평균은"+a/b+"입니다");
     }
 }
