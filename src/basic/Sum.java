49734f740dbc672a46f9e9baf00617028f95e762
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-02 19:32:22 +0900

    Update Sum.java

--- src/basic/Sum.java
+++ src/basic/Sum.java
@@ -3,15 +3,19 @@ public class babo{
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
         double sum = 0;
+        int Num=1;
         while(number.hasNext()){
             int num = number.nextInt();
             if(num == -1)
                 break;
             else
                 sum += num;
+            Num++ ;
         }
         System.out.println("숫자들의 합 :" + sum);
+        System.out.println("숫자들의 평균 :" + sum/Num);
 
     }
 
 }
+
