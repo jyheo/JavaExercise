ab9b7fba329d475adbcbd2499a8f08b3b5326672
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-02 19:23:43 +0900

    Update Sum.java
    배열을 통해서 5개의 숫자를 받아보았습니다.

--- src/basic/Sum.java
+++ src/basic/Sum.java
@@ -1,18 +1,17 @@
-package basic;
-
 import java.util.Scanner;
-
-public class Sum {
-    public static void main(String[] args) {
-        float fSum = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        fSum = scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
-        fSum += scanner.nextFloat();
-
-        System.out.println(fSum / 5);
+public class babo{
+    public static void main(String[] args){
+        Scanner number = new Scanner(System.in);
+        double sum = 0;
+        int intArray[] = new int[5];
+        System.out.println("5개의 숫자 입력 : ");
+        for(int i=0; i<intArray.length;i++){
+            intArray[i] = number.nextInt();
+        }
+        for(int i=0;i<intArray.length;i++){
+            sum += intArray[i];
+        }
+        System.out.println("숫자의 합: "+ sum);
+        System.out.println("숫자의 평균 :" + sum/intArray.length);
     }
 }
