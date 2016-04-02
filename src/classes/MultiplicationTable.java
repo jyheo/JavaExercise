eec05f7af197ab63e15d965fc9a1e04b61ba070e
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-02 19:56:23 +0900

    Update MultiplicationTable.java
    11단에서 19단  출력

--- src/classes/MultiplicationTable.java
+++ src/classes/MultiplicationTable.java
@@ -1,47 +1,10 @@
-package classes;
-
-/**
- * Created by jyheo on 2016-03-26.
- */
-public class MultiplicationTable {
-    private int number;
-    private int max_number = 9;
-
-    boolean setNumber(int num) {
-        if (1 <= num && num <= max_number) {
-            number = num;
-            return true;
-        } else {
-            return false;
+public class babo{
+    public static void main(String[] args){
+        int i,j;
+        for(i=11;i<20;i++, System.out.println()){
+            for(j=1;j<20;j++,System.out.print('\t')){
+                System.out.print(i + "*" + j + "=" + i*j);
+            }
         }
-    }
-
-    int getNumber() { return number; }
-
-    boolean setMaxNumber(int max_num) {
-        if (9 <= max_num && max_num <= 19) {
-            max_number = max_num;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    int getMaxNumber() { return max_number; }
-
-    void printTable() {
-        for (int i = 2; i <= max_number; i++) {
-            System.out.println(number + "x" + i + "=" + number * i);
-        }
-    }
-
-    public static void main(String[] args) {
-        MultiplicationTable mtable = new MultiplicationTable();
-
-        mtable.max_number = 19; // BAD practice!
-        mtable.setMaxNumber(19);
-
-        mtable.setNumber(4);
-        mtable.printTable();
     }
 }
