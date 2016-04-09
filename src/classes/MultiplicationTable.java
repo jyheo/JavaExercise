121a3f0a392d3c36a14e2cf2612bc1bcb7d0f8c3
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-09 13:27:48 +0900

    Update MultiplicationTable.java
    main함수변경

--- src/classes/MultiplicationTable.java
+++ src/classes/MultiplicationTable.java
@@ -1,9 +1,8 @@
-package classes;
 
 /**
  * Created by jyheo on 2016-03-26.
  */
-public class MultiplicationTable {
+public class babo {
     private int number;
     private int max_number = 9;
 
@@ -36,12 +35,11 @@ public class MultiplicationTable {
     }
 
     public static void main(String[] args) {
-        MultiplicationTable mtable = new MultiplicationTable();
-
-        mtable.max_number = 19; // BAD practice!
-        mtable.setMaxNumber(19);
-
-        mtable.setNumber(4);
-        mtable.printTable();
+        babo mtable = new babo();
+        for(int i=2;i<10;i++){
+            mtable.setNumber(i);
+            mtable.printTable();
+            System.out.println("\t");
+        }
     }
 }
