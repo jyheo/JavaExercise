c26d67eb225762592c9b820e1245f03e4086c015
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-12 19:35:45 +0900

    Update Lake10.java

--- src/classes/Lake10.java
+++ src/classes/Lake10.java
@@ -20,6 +20,51 @@ class FoolFish extends Fish {
     }
 }
 
+class Fish {
+    private String name;
+    private String shape;
+    protected int x, y;
+
+    public Fish(String name, String shape) {
+        this.name = name;
+        this.shape = shape;
+        x = 0;
+        y = 0;
+    }
+
+    public Fish() {
+        this.name = "Unknown";
+        this.shape = "<--<";
+        x = 0;
+        y = 0;
+    }
+
+    public void move(int width, int height) {
+        double rand = Math.random();
+        if (rand < 0.5)
+            x++;
+        else
+            y++;
+        if (x >= width)
+            x = 0;
+        if (y >= height)
+            y = 0;
+    }
+    public void move2(int width, int height){
+        x++;
+        y++;
+        if (x >= width)
+            x = 0;
+        if (y >= height)
+            y = 0;
+    }
+
+    public void display(int x, int y) {
+        if (this.x == x && this.y == y) {
+            System.out.print(shape);
+        }
+    }
+}
 public class Lake10 {
     private int width;
     private int height;
