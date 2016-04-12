5760263157a381324e637bcfa1da3d3a67d30c41
jeonjiyeon96 <jeonjiyeon96@naver.com>
2016-04-12 13:53:51 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,43 +1,55 @@
-package classes;
-
 public class Rectangle {
     private int x1, y1, x2, y2;
 
     public void setPoints(int _x1, int _y1, int _x2, int _y2) {
-        // TODO, check if x, y > 0
-        x1 = _x1;
-        y1 = _y1;
-        x2 = _x2;
-        y2 = _y2;
+        if (_x1 > 0 && _x2 > 0 && _y1 > 0 && _y2 > 0) {
+            x1 = _x1;
+            y1 = _y1;
+            x2 = _x2;
+            y2 = _y2;
+        }
     }
 
     public int getWidth() {
-        // TODO
-        return 0;
+        if(x1 > x2){
+            return x1-x2;
+        }
+        else{
+            return x2-x1;
+        }
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+        if(y1>y2){
+            return y1-y2;
+        }
+        else{
+            return y2-y1;
+        }
     }
 
     public int getArea() {
-        // TODO
-        return 0;
+        return getHeight() * getWidth();
     }
 
     public boolean equals(Rectangle r) {
-        // TODO
-        return false;
-    }
+       if(this.getArea()==r.getArea())
+               if(this.x1==r.x2 || this.x2==r.x1 || this.y1 == r.y2 || this.y2 ==r.y1)
+            return true;
+    return false;
+}
 
     public boolean in(Rectangle r) {
-        // TODO
-        return false;
+        if (this.x1 <= r.x1 || this.y1 <= r.y1 || this.x2 <= r.x2 || this.y2 <= r.y2)
+            return true;
+        else {
+            return false;
+        }
     }
 
     public boolean overlap(Rectangle r) {
-        // TODO
+        if(this.x2>=r.x2 || this.y1<=r.y1 || this.y2<=r.y2)
+            return true;
         return false;
     }
 
