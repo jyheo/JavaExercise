911b1f63121092093598e775bfe056cf978b7099
inmok <ko3036@gmail.com>
2016-05-22 18:15:48 +0900

    Update LakeAL.java

--- src/collections/LakeAL.java
+++ src/collections/LakeAL.java
@@ -70,10 +70,8 @@ class MyFish extends MyObject implements MoveDrawable {
 public class LakeAL {
     private int width;
     private int height;
-    private final int max_objects = 10;
-    private ArrayList<Drawable> drawables = new ArrayList<Drawable>();
-    private Movable[] movables = new Movable[max_objects];
-    private int movables_num = 0;
+    private ArrayList<MyObject> myobjects = new ArrayList<MyObject>();
+
 
     public LakeAL(int width, int height) {
         this.width = width;
@@ -81,25 +79,20 @@ public class LakeAL {
     }
 
     public void addMyObject(MyObject obj) {
-        if (obj instanceof Drawable) {
-            drawables.add((Drawable)obj);
-        }
-        if (obj instanceof Movable) {
-            addMovable((Movable)obj);
-        }
-    }
+        myobjects.add(obj);
 
-    public void addMovable(Movable m) {
-        if (movables_num >= max_objects)
-            return;
-        movables[movables_num++] = m;
     }
 
     public void moveObjects() {
-        for (int i = 0; i < movables_num; i++)
-            movables[i].move(width, height);
+        Iterator<MyObject> it = myobjects.iterator();
+        while (it.hasNext()) {
+            MyObject obj = it.next();
+            if (obj instanceof Movable) {
+                Movable m = (Movable) obj;
+                m.move(width, height);
+            }
+        }
     }
-
     public void display() {
         for (int i = 0; i < width; i++)
             System.out.print("-");
@@ -107,8 +100,10 @@ public class LakeAL {
         for (int i = 0; i < height; i++) {
             System.out.print("|");
             for (int j = 0; j < width; j++) {
-                for (Drawable d : drawables) {
-                    d.display(j, i);
+                for(MyObject obj : myobjects){
+                    if(obj instanceof Drawable){
+                        ((Drawable)obj).display(j,i);
+                    }
                 }
                 System.out.print(" ");
             }
